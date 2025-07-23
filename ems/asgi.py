a69e979117dd72a32790d4b62a0533b31aa25c51
chaitanya <150359158+chaitanya1718@users.noreply.github.com>
2025-07-23 15:05:30 +0530

    Update asgi.py

--- ems/asgi.py
+++ ems/asgi.py
@@ -1,17 +1,21 @@
 import os
 import django
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ems.settings")
-# django.setup()
+from channels.routing import get_default_application
 
-import records.routing 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ems.settings')
+django.setup() 
+
+import records.routing  
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  
-     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(records.routing.websocket_urlpatterns))
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            records.routing.websocket_urlpatterns
+        )
     ),
 })
