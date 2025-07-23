04a8187d08644c0051375ece6b3367fd4f27342f
chaitanya <150359158+chaitanya1718@users.noreply.github.com>
2025-07-23 14:50:30 +0530

    Update asgi.py

--- ems/asgi.py
+++ ems/asgi.py
@@ -3,15 +3,15 @@ import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
+from channels.security.websocket import AllowedHostsOriginValidator
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ems.settings")
 django.setup()
 
 from records.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # 👈 Add this line to support normal web browsing
-    "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+    "http": get_asgi_application(),  
+     "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(records.routing.websocket_urlpatterns))
     ),
 })
